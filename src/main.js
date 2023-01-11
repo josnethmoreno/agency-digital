fee468f1cdc87e6bf5d97408873c04ec2c11879d
josnethmoreno <josnethmoreno@gmail.com>
2023-01-10 23:56:21 -0400

    menu mobile

--- src/main.js
+++ src/main.js
@@ -1,2 +1,13 @@
 import './style.css'
 import './reset.css'
+
+const menuButton = document.querySelector('.menu-button')
+const menu = document.querySelector('.menu')
+const filterCloseMenu = document.querySelector('.menu-filter-black')
+
+const toggleMenu = () => {
+  menu.classList.toggle('show')
+}
+
+menuButton.addEventListener('click', toggleMenu)
+filterCloseMenu.addEventListener('click', toggleMenu)
