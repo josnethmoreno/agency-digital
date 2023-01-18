dc42ac3bcda3e48ab39fa535a752c206ec692692
josnethmoreno <josnethmoreno@gmail.com>
2023-01-18 17:17:44 -0400

    mobile responsive

--- src/main.js
+++ src/main.js
@@ -11,3 +11,15 @@ const toggleMenu = () => {
 
 menuButton.addEventListener('click', toggleMenu)
 filterCloseMenu.addEventListener('click', toggleMenu)
+
+
+const slider = document.querySelector('.blog-slider')
+const sliderContent = document.querySelector('.blog-slider-content')
+const sliderLeft = document.querySelector('.blog-slider-left')
+const sliderRight = document.querySelector('.blog-slider-right')
+
+const moveToRight = () => {
+  sliderContent.style.transform = translateX(100)
+}
+
+sliderRight.addEventListener('click', moveToRight)
