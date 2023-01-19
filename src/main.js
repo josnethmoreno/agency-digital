87c245933ea00330b77142c438fca8e74736ad00
josnethmoreno <josnethmoreno@gmail.com>
2023-01-19 01:35:26 -0400

    slider

--- src/main.js
+++ src/main.js
@@ -15,11 +15,40 @@ filterCloseMenu.addEventListener('click', toggleMenu)
 
 const slider = document.querySelector('.blog-slider')
 const sliderContent = document.querySelector('.blog-slider-content')
+const sliderLast = document.querySelector('.blog-card:last-of-type')
 const sliderLeft = document.querySelector('.blog-slider-left')
 const sliderRight = document.querySelector('.blog-slider-right')
 
+let sliderPosition = 0
+let sliderMovePX = 380;
+let sliderSize = sliderContent.getBoundingClientRect()
+
 const moveToRight = () => {
-  sliderContent.style.transform = translateX(100)
+  sliderContent.style.setProperty('transform', `translateX(${sliderPosition - 380}px)`)
+  sliderPosition -= sliderMovePX
+  calcPosition()
+  console.log(sliderPosition, sliderSize)
 }
 
+const moveToLeft = () => {
+  sliderContent.style.setProperty('transform', `translateX(${sliderPosition + 380}px)`)
+  sliderPosition += sliderMovePX
+  calcPosition()
+  console.log(sliderPosition)
+}
+
+const calcPosition = () => {
+  if(sliderPosition === 0) { 
+    sliderLeft.disabled = true
+    return
+   }
+
+   sliderLeft.disabled = false
+}
+
+calcPosition()
+
 sliderRight.addEventListener('click', moveToRight)
+sliderLeft.addEventListener('click', moveToLeft)
+
+console.log(sliderLast.style.display = 'none')
