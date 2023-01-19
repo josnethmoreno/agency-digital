f97b6c4e89d07cb70d829fa4c30eed7848e9fbbc
josnethmoreno <josnethmoreno@gmail.com>
2023-01-19 02:12:00 -0400

    slider

--- src/main.js
+++ src/main.js
@@ -3,35 +3,44 @@ import './reset.css'
 
 const menuButton = document.querySelector('.menu-button')
 const menu = document.querySelector('.menu')
+const menuItem = document.querySelectorAll('.menu-item')
 const filterCloseMenu = document.querySelector('.menu-filter-black')
 
 const toggleMenu = () => {
   menu.classList.toggle('show')
 }
 
+const closeMenu = () => {
+  menu.classList.remove('show')
+}
+
 menuButton.addEventListener('click', toggleMenu)
 filterCloseMenu.addEventListener('click', toggleMenu)
+menuItem.forEach((item) => {
+  item.addEventListener('click', closeMenu)
+})
 
 
-const slider = document.querySelector('.blog-slider')
+
 const sliderContent = document.querySelector('.blog-slider-content')
 const sliderLast = document.querySelector('.blog-card:last-of-type')
 const sliderLeft = document.querySelector('.blog-slider-left')
 const sliderRight = document.querySelector('.blog-slider-right')
 
 let sliderPosition = 0
-let sliderMovePX = 380;
-let sliderSize = sliderContent.getBoundingClientRect()
+let slideWidth = sliderLast.clientWidth + 16
+let sliderMovePX = slideWidth;
+
+console.log(slideWidth)
 
 const moveToRight = () => {
-  sliderContent.style.setProperty('transform', `translateX(${sliderPosition - 380}px)`)
+  sliderContent.style.setProperty('transform', `translateX(${sliderPosition - slideWidth}px)`)
   sliderPosition -= sliderMovePX
   calcPosition()
-  console.log(sliderPosition, sliderSize)
 }
 
 const moveToLeft = () => {
-  sliderContent.style.setProperty('transform', `translateX(${sliderPosition + 380}px)`)
+  sliderContent.style.setProperty('transform', `translateX(${sliderPosition + slideWidth}px)`)
   sliderPosition += sliderMovePX
   calcPosition()
   console.log(sliderPosition)
@@ -40,15 +49,31 @@ const moveToLeft = () => {
 const calcPosition = () => {
   if(sliderPosition === 0) { 
     sliderLeft.disabled = true
-    return
+    sliderRight.disabled = false
+   return
    }
-
    sliderLeft.disabled = false
-}
+  }
 
 calcPosition()
 
 sliderRight.addEventListener('click', moveToRight)
 sliderLeft.addEventListener('click', moveToLeft)
 
-console.log(sliderLast.style.display = 'none')
+const toShowLast = (entries) => {
+  entries.forEach((entry) => {
+    if(entry.isIntersecting) {
+      sliderRight.disabled = true
+    } else {
+      sliderRight.disabled = false
+    }
+  })
+}
+
+const observerLast = new IntersectionObserver(toShowLast, {
+  root: null,
+  rootMargin: '0px',
+  threshold: .2
+})
+
+observerLast.observe(sliderLast)
